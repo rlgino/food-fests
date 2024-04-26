4984d095478bcb46b2b696117ffeda83c8bf7ac2
Gino Luraschi <ext_rgluraschi@falabella.cl>
2024-04-26 13:26:59 -0300

    fix: Rename groups

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -3,20 +3,20 @@ import firebaseApp from "./firebase"
 
 const db = getDatabase(firebaseApp)
 
-export const GROUP_POPULARES = "populares"
-export const GROUP_DIVINAS = "divinas"
+export const GROUP_CHETAS = "chetas"
+export const GROUP_MILIPILIS = "milipilis"
 
 export const selectGroup = async () => {
 
     const groupsRef = ref(db, 'groups/');
-    const group1 = await get(child(groupsRef, GROUP_POPULARES))
-    const group2 = await get(child(groupsRef, GROUP_DIVINAS))
+    const group1 = await get(child(groupsRef, GROUP_CHETAS))
+    const group2 = await get(child(groupsRef, GROUP_MILIPILIS))
     let group1Size = group1.exists() ? group1.val().length : 0
     let group2Size = group2.exists() ? group2.val().length : 0
 
-    if (group1Size - group2Size > 1) return GROUP_DIVINAS
-    else if (group2Size - group1Size > 1) return GROUP_POPULARES
-    else return Math.random() > 0.5 ? GROUP_DIVINAS : GROUP_POPULARES
+    if (group1Size - group2Size > 1) return GROUP_MILIPILIS
+    else if (group2Size - group1Size > 1) return GROUP_CHETAS
+    else return Math.random() > 0.5 ? GROUP_MILIPILIS : GROUP_CHETAS
 }
 
 export const savePerson = (name, group) => {
